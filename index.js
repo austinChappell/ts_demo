a0f51c4b4f2b1196eb528497833a9f85690aa583
Austin Chappell <achappell@triumphpay.com>
2019-09-09 20:32:37 -0500

    compile

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 // Simple Types
 var message = 'Hello World';
 var isLoading = true;
@@ -6,13 +17,26 @@ var amount = 245;
 var userIds = [1, 2, 3, 4, 5, 6];
 var ids = [1, 2, 3, 4, 5, 6];
 // Functions
-var handleClick = function () {
+// Okay
+var setSomeId = function (id) {
     // do something
 };
-var otherHandleClick = function () {
+var setId = function (id) {
     // do something
 };
-var user = {
+var stringValueSetter = function (someString) {
+    // do something
+};
+var numValueSetter = function (someNum) {
+    // do something
+};
+var boolValueSetter = function (someBool) {
+    // do something
+};
+var defaultValueSetter = function (id) {
+    // do something
+};
+var user2 = {
     age: 34,
     id: 2,
     name: 'Austin'
@@ -22,6 +46,9 @@ var fontColors = {
     primary: '#231323',
     secondary: '#6b29a3'
 };
+var setUser = function (user) {
+    // do something with the user object
+};
 // Enums
 var Responses;
 (function (Responses) {
@@ -29,8 +56,8 @@ var Responses;
     Responses[Responses["Yes"] = 1] = "Yes";
     Responses[Responses["Maybe"] = 2] = "Maybe";
 })(Responses || (Responses = {}));
-console.log('Response[0] =', Responses[0]);
-console.log('Response.No =', Responses.No);
+console.log('Response[0] =', Responses[0]); // No
+console.log('Response.No =', Responses.No); // 0
 var KeyCodes;
 (function (KeyCodes) {
     KeyCodes[KeyCodes["Enter"] = 13] = "Enter";
@@ -39,5 +66,14 @@ var KeyCodes;
     KeyCodes[KeyCodes["Shift"] = 16] = "Shift";
     KeyCodes[KeyCodes["Escape"] = 27] = "Escape";
 })(KeyCodes || (KeyCodes = {}));
-console.log('KeyCodes[13] =', KeyCodes[13]);
-console.log('KeyCodes.Enter =', KeyCodes.Enter);
+console.log('KeyCodes[13] =', KeyCodes[13]); // Enter
+console.log('KeyCodes.Enter =', KeyCodes.Enter); // 13
+// Redux Actions
+var actionTypes = {
+    GET_USER: 'GET_USER',
+    SET_USER: 'SET_USER'
+};
+var actions = {
+    GET_USER: function (state, action) { return (__assign({}, state)); },
+    SET_USER: function (state, action) { return (__assign({}, state)); }
+};
